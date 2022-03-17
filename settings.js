58d73e57b9a5ad3ce56fb038284ea73e07426ccc
wran-Md <101816722+wran-Md@users.noreply.github.com>
2022-03-18 00:31:18 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,9 +30,9 @@ global.ownernomerr = "+62895326205615" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["62895326205615"] //ganti agar fitur owner bisa di gunakan
 global.packname = 'Wranzzz' //sticker wm ubah
-global.author = 'G4NZ' //sticker wm ganti nama kalian
+global.author = '4Youuu' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
